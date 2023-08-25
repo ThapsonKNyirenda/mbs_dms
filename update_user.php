27a8084f7319ea17456c443c74a69a30849b8363
ThapsonKNyirenda <thapsonknyirenda@gmail.com>
2023-08-24 17:06:54 -0700

    have aded new files

---
+++ update_user.php
@@ -0,0 +1,33 @@
+<?php
+  session_start();
+?>
+<?php
+
+    if (!isset($_SESSION['id'])) {
+      header('location: index.php');
+    }
+
+    include('connection/connection.php');
+?>
+
+<?php
+
+        $fName=$_POST['fName'];
+        $lName=$_POST['lName'];
+        $department=$_POST['department'];
+        $role=$_POST['role'];
+        $email=$_POST['email'];
+        $password=$_POST['password'];
+
+		
+        $query = "UPDATE `users` SET `fName` = '$fName',`lName` = '$lName',`email` = '$email',`password` = '$password',`department` = '$department'
+        WHERE `id` = ".$_GET['doc_id'];
+
+        $result = mysqli_query($conn, $query);
+		
+?>	
+	<script type="text/javascript">
+            alert("Editing Completed Successfully!");
+            window.location = "usermanagement.php";
+                                
+	</script>
